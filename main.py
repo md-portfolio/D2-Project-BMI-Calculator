8b541aee448105d1bef3bfb25ff77c938138dd93
md-portfolio <88002533+md-portfolio@users.noreply.github.com>
2022-09-18 22:00:44 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# 
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+# Input conversion
+height_float = float(height)
+weight_int = int(weight)
+
+# Using the exponent operator **
+bmi = weight_int / height_float ** 2
+# Using multiplication and PEMDAS
+bmi = weight_int / (height_float * height_float)
+
+# Output conversion
+bmi_int = int(bmi)
+
+# F-string
+print(f'Your BMI is ' f'{bmi_int}')
